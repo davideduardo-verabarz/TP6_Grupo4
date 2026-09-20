14c0da0f5b047ac363683eee79aac9971b8d224e
davideduardo-verabarz <davidverabarz2017@gmail.com>
2026-09-20 01:36:42 -0300

    creacion del proyecto
    Se creo el proyecto completo, se agrego el archivo jframe, la clase producto, se realizo toda la parte visual, se modifico la tabla para completar y se cargo java enum con categoria para trabajar jcombobox.

---
+++ GestionDeProductos/src/gestiondeproductos/Categoria.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package gestiondeproductos;
+
+/**
+ *
+ * @author Usuario
+ */
+public enum Categoria {
+    LIMPIEZA,
+    ELECTRONICA,
+    INDUMENTARIA,
+    BAZAR,
+    FARMACIA,
+}
---
+++ GestionDeProductos/src/gestiondeproductos/Producto.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package gestiondeproductos;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Producto {
+    private String nombre;
+    private Categoria categoria;
+    private double precio;
+
+    public Producto(String nombre, Categoria categoria, double precio) {
+        this.nombre = nombre;
+        this.categoria = categoria;
+        this.precio = precio;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+    
+    
+}
